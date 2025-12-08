849483ad83217654229dbbcc00e50761b790d45f
RIOKOWI <achyarrio2004@gmail.com>
2025-12-08 20:05:19 +0700

    LIST: ubah isi data

--- bin/fixed_list.dart
+++ bin/fixed_list.dart
@@ -1,6 +1,18 @@
 void main(){
-  var list = List<int>.filled(10, 1);
+  var list = List<int>.filled(10, 0);
   print(list);
 
-  // list.add(10); // error
+  // list.add(10); // error karena sudah fix isinya 
+
+  list[1] = 1;
+  list[2] = 2;
+  list[3] = 3;
+  list[4] = 4;
+  list[5] = 5;
+  list[6] = 6;
+  list[7] = 7;
+  list[8] = 8;
+  list[9] = 9;
+
+  print(list);
 }
