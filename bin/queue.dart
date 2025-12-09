a1d7e62de2d56ce86ff8889f84bf2096df189bbf
RIOKOWI <achyarrio2004@gmail.com>
2025-12-09 19:26:26 +0700

    LIST QUEUE: contoh kode queue last in first out

--- bin/queue.dart
+++ bin/queue.dart
@@ -14,4 +14,20 @@ void main(){
   print(fifo.removeFirst());
 
   print(fifo); // udah gak ada antrian
+
+
+  // LAST IN FIRST OUT
+  final lafo = Queue<String>();
+
+  // last in
+  lafo.addLast('kue');
+  lafo.addLast('tepung');
+  lafo.addLast('aci');
+
+  // first out
+  print(lafo.removeLast());
+  print(lafo.removeLast());
+  print(lafo.removeLast());
+
+  print(lafo);
 }
